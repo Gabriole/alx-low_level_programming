d2be229f27ed2dbbe745409c0e0f6da6dc34496d
Gabriole <ismail.mouhtaram@gmail.com>
2023-12-19 09:20:00 -0800

    pushingC

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -8,10 +7,10 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
-{
+{	
 	if (n > 0)
 	{
-       	_putchar(43);
+	_putchar(43);
 	return (1);
 	}
 	else if (n < 0)
